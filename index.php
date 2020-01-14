b59d057224b13a9c5023fc5b9cd577ddd196e4ed
LynneKinyua <lynnemkin@gmail.com>
2020-01-14 22:37:24 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+ <?php
+         class Students
+        {
+ public function __construct()
+ /**
+ * Code to be executed
+ *upon object instantiation
+ */
+        }
+     
+ ?>
+
+ </body>
+</html>
