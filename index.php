6a81728bff6f308331d7f1e849b4d16fec771d82
LynneKinyua <58708074+LynneKinyua@users.noreply.github.com>
2020-01-15 22:16:01 +0300

    update

--- index.php
+++ index.php
@@ -1,19 +1,14 @@
-<html>
- <head>
-  <title>PHP Test</title>
- </head>
- <body>
- <?php
-         class Students
-        {
- public function __construct()
- /**
- * Code to be executed
- *upon object instantiation
- */
-        }
-     
- ?>
+<?php
+  // Declare the variable 'string' and assign it a value.
+  // The <br> is the HTML equivalent to a new line.
+  $string = 'Hello World!<br>';
 
- </body>
-</html>
+  // You can echo the variable, similar to the way you would echo a string.
+  echo $string;
+
+  // You could also use print.
+  print $string;
+
+  // Or, if you are familiar with C, printf can be used too.
+  printf('%s', $string);
+?>
